5aa6c1657e675200b8518fa335bf5c7a07c2bf47
Alex Polotsky <polonyc77@gmail.com>
2017-04-29 19:54:49 -0400

    updates

--- nycda-jukebox-master/js/script.js
+++ nycda-jukebox-master/js/script.js
@@ -4,14 +4,18 @@
  * JUKEBOX SINGLETON
  */
 var Jukebox = {
+	images: [],
 	songs: [],
 	activeSong: null,
-	volume: 100,
+	volume: 0,
 	isPlaying: false,
 	dom: {},
+	
 
 	// Run this function to kick the whole thing off
 	start: function() {
+		// Initialize the SoundCloud API
+ 		SC.initialize({ client_id: "fd4e76fc67798bfa742089ed619084a6" });
 		// Grab the dom elements
 		this.dom = {
 			play: $(".jukebox-controls-play"),
@@ -19,13 +23,13 @@ var Jukebox = {
 			next: $(".jukebox-controls-next"),
 			prev: $(".jukebox-controls-previous"),
 			myRange: $("#myRange"),
-			shuffle: $(".shuffle"),
+			shuffle: $(".jukebox-controls-shuffle"),
 			upload: $(".jukebox-header-upload"),
+            input: $(".input1"),
 			songs: $(".jukebox-songs"),
-		    dur: $("#dur"),
+		    albumArt: $(".albumArt"),
 		};
 
-		// Queue up some default songs
 		this.addSong("./songs/Zimbabwe.mp3", {
 			title: "Can I Get Wit' Ya in Zimbabwe",
 			artist: "Notorious B.I.G. / New Navy",
@@ -39,9 +43,17 @@ var Jukebox = {
 			artist: "Tycho",
 		});
 		
+        this.addSong("./songs/CoastalBrake.mp3", {
+			title: "Coastal Brake",
+			artist: "Tycho",
+		});
+     
+        this.addSong("https://soundcloud.com/viceroymusic/50-cent-disco-inferno-viceroy-jet-life-remix"); 
+        
         this.change(this.songs[0]);
 
 		// Render and bind!
+		
 		this.render();
 		this.listen();
 	},
@@ -56,13 +68,13 @@ var Jukebox = {
 			}
 		}.bind(this));
 
-		this.dom.myRange.on("change", function() {
+		this.dom.myRange.on("mousemove", function() {
 	
 			this.setVolume(0);
 		}.bind(this));
 
 		this.dom.next.on("click", function() {
-			this.skip(1);
+			this.skip(1).play();
 		}.bind(this));
 
 		this.dom.prev.on("click", function() {
@@ -72,11 +84,17 @@ var Jukebox = {
 		this.dom.stop.on("click", this.stop.bind(this));
 
         this.dom.shuffle.on("click", function() {
-             
-             this.shuffle();
+             this.shuffle(this.songs);
            }.bind(this));
 		
-		this.dom.upload.on("change", function() {
+		
+            
+        this.dom.input.on("input", function() {
+        	this.addSong(this.dom.input.val());
+         console.log(this);
+        }.bind(this));
+
+	      this.dom.upload.on("change", function() {
 			var files = this.dom.upload.prop("files");
 
 			for (var i = 0; i < files.length; i++) {
@@ -84,6 +102,7 @@ var Jukebox = {
 				this.addSong(file, {
 					title: "Uploaded song",
 					artist: "Unknown",
+				    
 				});
 			}
 		}.bind(this));
@@ -91,20 +110,38 @@ var Jukebox = {
 
 	render: function() {
 		// Render song elements
-		this.dom.songs.html("");
+		 // this.dom.songs.html("");
 		for (var i = 0; i < this.songs.length; i++) {
 			var $song = this.songs[i].render();
-			this.dom.songs.append($song);
-
+			
 			if (this.songs[i] === this.activeSong) {
 				$song.addClass("isActive");
 			}
+		    else {
+		    	$song.removeClass("isActive");
+		    }
 		}
+
+		
 
 		// Indicate paused vs played
 		this.dom.play.toggleClass("isPlaying", this.isPlaying);
 		this.dom.stop.toggleClass("isDisabled", !this.isPlaying);
+	
+	
+
+     if (this.activeSong && this.activeSong.meta.image) {
+           this.dom.albumArt.prop("src", this.activeSong.meta.image);
+        }
+        else {
+            this.dom.albumArt.prop("src", "");
+         }
+         
+
 	},
+
+	
+    
 
 	play: function(song) {
 		if (song) {
@@ -117,6 +154,7 @@ var Jukebox = {
 
 		this.isPlaying = true;
 		this.activeSong.play();
+		this.setVolume(this.volume)
 		this.render();
 		return this.activeSong;
 	},
@@ -158,65 +196,87 @@ var Jukebox = {
 			return false;
 		}
 
-		// Find the current song's index
 		var idx = this.songs.indexOf(this.activeSong);
 
-		// Set the desired index by adding the direction, and limiting it to the
-		// length of the array using a modulous operator
+		
 		var desiredIndex = (idx + direction) % this.songs.length;
 
 		// Change to the desired song
 		return this.change(this.songs[desiredIndex]);
 	},
 
-	
-
-	shuffle: function() {
-		
-		console.log("Jukebox is shuffleing");
-	},
 
 	addSong: function(file, meta) {
-		var song = new Song(file, meta);
-		this.songs.push(song);
-		this.render();
-		return song;
-	},
+	    var song;
+ 
+ 		if (file.indexOf("soundcloud.com") !== -1) {
+ 			song = new SoundCloudSong(file);
+ 		}
+ 		else {
+ 			song = new FileSong(file, meta);
+ 		}
+  		this.songs.push(song);
+        var $song = song.render();
+		this.dom.songs.append($song);
+		
+  		
+  		this.render();
+  		return song;
+ 	},
+		
+		
 
 	// volumeLevel should be a number between 0-100
 	setVolume: function(volumeLevel) {
-	this.activeSong.volume(this.volumeLevel = myRange.value / 100);
-    }, 
+	this.activeSong.volume(this.volume = myRange.value / 100);
+     
+ }, 
      
   
-
+shuffle: function() {
+     this.songs.sort(function() { 
+     return	Math.floor(Math.random() * 3 - 1);
+     });
+     
+     this.change(this.songs[0]);
+     this.play();
+   
+   console.log("Jukebox is shuffleing");
+	},
+   
 };
 
-    
-
-/**
+  /**
  * SONG CLASS
  */
 class Song {
-	constructor(file, meta) {
-		this.file = file;
-		this.meta = meta || {
-			title: "Unknown title",
-			artist: "Unknown artist",
-		};
-		this.audio = new Audio(file);
-	}
-      
+  
+ 	constructor() {
+ 		this.file = null;
+ 		this.meta = {};
+ 			 this.audio = null;
+ 			 this.$song = $("<div class='jukebox-songs-song'></div>");
+ 			
+ 		}
+ 		
+	///
 	render() {
-		var $song = $('<div class="jukebox-songs-song"></div>');
-		$song.append('<div class="jukebox-songs-song-pic"></div>');
-		$song.append('<div class="jukebox-songs-song-title">' + this.meta.title + '</div>');
-		$song.append('<div class="jukebox-songs-song-artist">' + this.meta.artist + '</div>');
-		$song.append('<div class="jukebox-songs-song-duration">' + "3:22" + '</div>');
-		$song.data("song", this);
-		return $song;
+        this.$song.html("");
+		this.$song.append('<div class="jukebox-songs-song-pic"></div>');
+		this.$song.append('<div class="jukebox-songs-song-title">' + this.meta.title + '</div>');
+		this.$song.append('<div class="jukebox-songs-song-artist">' + '<a href= '+this.meta.user+'>' + this.meta.artist + '</div>');
+		this.$song.append('<img class="soundcloud-image albumArt" src = '+ this.meta.image+'>');
+		this.$song.append('<div class="jukebox-songs-song-duration"></div>');
+		
+		this.$song.data("song", this);
+	
+		return this.$song;
+	    
 	}
- 
+
+
+    
+
 	play() {
 		this.audio.play();
 	}
@@ -233,12 +293,68 @@ class Song {
 	    this.audio.volume = volumeLevel;
 	    
 	  }
-	   
-    
-	
-};
+  }   
+ class FileSong extends Song {
+   
+ constructor(file, meta) {	
+ 	 super();
+ 	 this.file = file;
+ 	 this.meta = meta || {
+ 	      title: "Unknown title",
+ 	      artist: "Unknown artist",
+    };    
+    this.audio = new Audio(file);
+ }	
+}
+
+class SoundCloudSong extends Song {
+
+constructor (url) {
+	super();
+        
+	    SC.resolve(url)
+           .then(function(song) {
+               this.meta = {
+               	   title: song.title,
+                   artist: song.user.username,
+                   image:  song.artwork_url,
+                   user: song.permalink_url,
+               };
+             return song;
+         }.bind(this))
+      .then(function(song) {
+      	   var uri = song.uri + "/stream?client_id=fd4e76fc67798bfa742089ed619084a6";
+      	   this.audio = new Audio(uri);
+          
+
+      }.bind(this))
+
+       .then(function() {
+       	    this.render();
+       }.bind(this));
+ 
+    }
+}
 
 
 $(document).ready(function() {
 	Jukebox.start();
+	
 });
+        
+		
+		
+
+		
+             
+     
+     
+
+ 
+
+    	
+	
+
+	
+
+	
